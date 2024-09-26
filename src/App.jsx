099270572b0e62a1003a167e0000acd62c30eb46
Evan Ward <Evan29Ward@gmail.com>
2024-09-26 17:38:37 +0100

    Built app using components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import Header from "./components/Header.jsx";
+import About from "./components/About.jsx";
+import Projects from "./components/projects/Projects.jsx";
+import Skills from "./components/skills/Skills.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <Projects/>
+            <About/>
+            <Skills/>
+            {/*Copyright Section*/}
+            <div className="copyright py-4 text-center text-white">
+                <div className="container"><small>Copyright &copy; Evan Ward {(new Date()).getFullYear()}</small></div>
+            </div>
+        </>
+    )
 }
 
 export default App
